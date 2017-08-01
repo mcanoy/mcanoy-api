92c5f101b46feedca7e95844230db9270671c558
Kevin McAnoy <kmcanoy@redhat.com>
2017-08-01 11:37:13 -0700

    Default to json

---
+++ src/main/java/com/mcanoy/config/RestConfig.java
@@ -0,0 +1,22 @@
+package com.mcanoy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.http.MediaType;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return new RepositoryRestConfigurerAdapter() {
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+                config.setDefaultMediaType(MediaType.APPLICATION_JSON);
+            }
+        };
+    }
+}
