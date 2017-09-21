7c6bec394c7ce4c0ae57416bb64afeda5b7cad40
Kevin McAnoy <kmcanoy@redhat.com>
2017-09-21 10:20:50 -0700

    Query set by name

--- src/main/java/com/mcanoy/data/PictureSetRepository.java
+++ src/main/java/com/mcanoy/data/PictureSetRepository.java
@@ -1,6 +1,7 @@
 package com.mcanoy.data;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -17,4 +18,7 @@ public interface PictureSetRepository extends PagingAndSortingRepository<Picture
     @Override
     @RestResource(exported = false)
     public void delete(PictureSet set);
+    
+    @RestResource(path = "name", rel = "name")
+    public PictureSet findByName(@Param("name") String name);
 }
