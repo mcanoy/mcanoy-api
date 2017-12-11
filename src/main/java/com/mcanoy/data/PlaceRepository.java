0f54a1a5c83e09152e5d4d6873baac41504664d9
Kevin McAnoy <kmcanoy@redhat.com>
2017-12-10 18:00:49 -0800

    Add find place by name

--- src/main/java/com/mcanoy/data/PlaceRepository.java
+++ src/main/java/com/mcanoy/data/PlaceRepository.java
@@ -1,6 +1,7 @@
 package com.mcanoy.data;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -18,4 +19,7 @@ public interface PlaceRepository extends PagingAndSortingRepository<Place, Long>
     @Override
     @RestResource(exported = false)
     public void delete(Place set);
+    
+    @RestResource(path = "name", rel = "name")
+    public Place findByName(@Param("name") String name);
 }
